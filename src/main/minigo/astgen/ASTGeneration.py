6f98ca729726415c44c7fda6ae9df08c2076e733
Khoa Le Van Anh <levananhkhoa2004@gmail.com>
2025-09-19 08:34:32 +0700

    Clean up initial comments in ASTGeneration.py
    Removed initial comments and author information.

--- src/main/minigo/astgen/ASTGeneration.py
+++ src/main/minigo/astgen/ASTGeneration.py
@@ -1,11 +1,4 @@
-"""
- * Initial code for Assignment 1, 2
- * Programming Language Principles
- * Author: Võ Tiến
- * Link FB : https://www.facebook.com/Shiba.Vo.Tien
- * Link Group : https://www.facebook.com/groups/khmt.ktmt.cse.bku
- * Date: 20.01.2025
-"""
+
 from MiniGoVisitor import MiniGoVisitor
 from MiniGoParser import MiniGoParser
 from AST import *
